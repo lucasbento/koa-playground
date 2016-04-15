3b8e5691f52532b6f05e56a443d0f8b4846a4770
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-15 20:28:02 -0300

    Ex. 4/11 - Added koa-bodyparser & worked with JSON / stream files in middlewares

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-var koa = require('koa');
-var app = koa();
-var parse = require('co-body');
+var app = require('koa')();
+var bodyParser = require('koa-bodyparser');
+var router = require('koa-router')();
+var fs = require('fs');
 
 var port = process.argv[2] || 4000;
 
-app.listen(port);
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(port);
 
-app.use(function* (next) {
-  if (this.method === 'POST') {
-    var body = yield parse(this);
-
-    if (body.hasOwnProperty('name')) {
-      body.name = body.name.toUpperCase();
-      this.body = body.name;
-    }
-  } else if (this.method === 'GET') {
-    if (this.path === '/') {
-      return this.body = 'hello koa';
-    } else if (this.path === '/404') {
-      return this.body = 'page not found';
-    } else if (this.path === '/500') {
-      return this.body = 'internal server error';
-    }
-  }
-
-  yield next;
-});
+router
+  .get('/json', function *() {
+    this.body = { foo: 'bar' };
+  })
+  .get('/stream', function *() {
+    var file = process.argv[3];
+    this.body = fs.createReadStream(file);
+  });
