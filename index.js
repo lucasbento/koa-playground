7638ef13cb418bab14f33d5c25aa33355891f1d7
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-16 09:01:00 -0300

    Ex. 5/11: Played with content-types

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 var app = require('koa')();
-var bodyParser = require('koa-bodyparser');
-var router = require('koa-router')();
-var fs = require('fs');
+// var bodyParser = require('koa-bodyparser');
+// var router = require('koa-router')();
+// var fs = require('fs');
 
 var port = process.argv[2] || 4000;
 
 app
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(port);
-
-router
-  .get('/json', function *() {
-    this.body = { foo: 'bar' };
+  // .use(bodyParser())
+  // .use(router.routes())
+  // .use(router.allowedMethods())
+  .use(function* () {
+    this.body = (this.request.is('application/json')) ? { message: 'hi!' } : 'ok';
   })
-  .get('/stream', function *() {
-    var file = process.argv[3];
-    this.body = fs.createReadStream(file);
-  });
+  .listen(port);
