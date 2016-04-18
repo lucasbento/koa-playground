8d917116ac2edb4e24d02086f90f66b7c38b8509
Lucas Bento <lucas.bsilva@outlook.com>
2016-04-18 15:52:34 -0300

    Finished - authentication!

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 var app = require('koa')();
+var bodyParser = require('koa-bodyparser');
+var session = require('koa-session');
 var views = require('co-views');
-
 var render = views(__dirname + '/views', {
 	ext: 'ejs'
 });
 
-app.keys = ['koa', 'secret', 'keys', 'for', 'sessions'];
+app.keys = ['secret', 'secure', 'keys'];
 
 app
-	.use(function* (next) {
-		var user = {
-			name: {
-				first: 'Tobi',
-				last: 'Holowaychuk'
-			},
-			species: 'ferret',
-			age: 3
+	.use(session(app))
+	.use(bodyParser())
+	.use(function* home(next) {
+		if (this.request.path !== '/')
+			return yield next;
+
+		if (this.session.loggedIn) {
+			this.status = 200;
+			return this.body = 'hello world';
 		}
 
-		this.body = yield render('user', { user: user });
+		this.status = 401;
+	})
+	.use(function* login(next) {
+		if (this.request.path !== '/login')
+			return yield next;
+
+		if (this.request.method === 'GET')
+			return render('form');
+
+		if (this.request.body.username === 'username' && this.request.body.password === 'password') {
+			this.session.loggedIn = true;
+			return this.redirect('/');
+		}
+
+		this.status = 400;
+	})
+	.use(function* logout(next) {
+		if (this.request.path !== '/logout')
+			return yield next;
+
+		this.session.loggedIn = false;
+		this.redirect('/login');
 	})
 	.listen(process.argv[2] || 4000);
